425ba3cefcf9df5811afbead16248868153e0c49
bigbooss <bannaniferes@gmail.com>
2020-10-11 17:43:10 +0200

    tp1 manque le probleme de permission

---
+++ TP1_BANNANI_LOUMI/app/src/main/java/com/example/tp1_bannani_loumi/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.tp1_bannani_loumi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    boolean numeroValide;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendSMS(View view) {
+        numeroValide = true;
+        EditText editNumeros = (EditText) findViewById(R.id.editNumero);
+        EditText editMessage = (EditText) findViewById(R.id.editMesage);
+        String numeros = editNumeros.getText().toString();
+        String message = editMessage.getText().toString();
+        if (message.length() > 0) {
+            if (numeros.contains(";")) {
+                String[] tableau = numeros.split(";");
+                for (int i = 0; i < tableau.length; i++) {
+                    if (tableau[i].length() < 4) {
+                        Toast.makeText(getApplicationContext(), "Le numero " + tableau[i] + " est invalide", Toast.LENGTH_SHORT).show();
+                        numeroValide = false;
+                    }
+                }
+                if (numeroValide) {
+                    for (int i = 0; i < tableau.length; i++) {
+                        SmsManager.getDefault().sendTextMessage(tableau[i], null, message, null, null);
+                    }
+                }
+            } else if (numeros.length() >= 4) {
+                SmsManager.getDefault().sendTextMessage(numeros, null, message, null, null);
+            } else {
+                Toast.makeText(getApplicationContext(), "Le numero " + numeros + " est invalide", Toast.LENGTH_SHORT).show();
+                numeroValide = false;
+            }
+            if (numeroValide) {
+                editNumeros.setText("");
+                editMessage.setText("");
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Entrer le message", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
